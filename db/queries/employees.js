6511ec0df05479f8e64268959c6f896aaacc7768
Twinkle <twinkledevwanshi93@gmail.com>
2025-08-29 18:19:49 -0700

    write the function

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,7 +1,24 @@
+import db from "#db/client";            
+
+
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
   // TODO
+try {
+  const sql = `
+  insert into employees(name, birthday, salary)
+  values($1,$2,$3)
+  returning * `;
+  const values = [ name, birthday, salary];
+
+  const res = await db.query(sql,values);
+  return res.rows[0];
+  
+} catch (error) {
+  console.error(error);
 }
+    
+};
 
 // === Part 2 ===
 
